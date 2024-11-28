2fafc4fcbc1625575735860fb5849adb87287f50
tyagaraj_s_d <112246965+tyagaraj-raj-2007@users.noreply.github.com>
2024-11-28 21:52:11 +0530

    Add files via upload

---
+++ Chikkamagaluru/js/custom.js
@@ -0,0 +1,37 @@
+// ===== Scroll to Top ==== 
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200); // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200); // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function () {
+    // When arrow is clicked
+
+    $('body, html').animate({
+        scrollTop: 0 // Scroll to top of body
+    }, 500);
+});
+// for FAQ //
+
+const accordionBtns = document.querySelectorAll(".item-header");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    this.classList.toggle("active");
+
+    let content = this.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
+  };
+});
+
